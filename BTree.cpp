98494bd016ed210828f15bdbd2917ef1cac98550
Tristan Starck <ttstarck@gmail.com>
2016-03-11 18:08:15 -0800

    Split InternalNode still in progress (9)

--- BTree.cpp
+++ BTree.cpp
@@ -162,7 +162,7 @@ void BTree::splitInternalNode(InternalNode* firstInternalNode){
   }
   
   firstInternalNode->names[M/2]="no name index";
-  if(firstInternalNode->leaves[0] != NULL) std::cout<< "Leaves is NULL\n";  
+  if(firstInternalNode->leaves[0]->items != NULL) std::cout<< "Leaves is NOT NULL\n";  
   //if the node is the root then make a new root
   if(firstInternalNode==root){
     std::cout<<"adding a new root"<<std::endl;
