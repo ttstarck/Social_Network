ed84532fe7a9a97efe1e0c898dba63ccb23bc3db
Kelly Bielaski <kellybielaski@umail.ucsb.edu>
2016-03-12 22:17:35 -0800

    enabled reading in a file through the command line

--- main.cpp
+++ main.cpp
@@ -155,46 +155,107 @@ int main(){
 	    b->printBTree();
 
 	  }
+	else if(str.compare("insertFile") == 0)
+          {
+	    std::string fileName;
+	    cin >> fileName;
+	    ifstream file(fileName);
+
+	    //parse through the input.txt line by line
+	    while(getline(file,line)){
+	      string name = "";
+	      string occupation = "";
+	      string age = "";
+
+	      FriendNode* friendHead = new FriendNode;
+	      friendHead->name = "";
+	      friendHead->nextFriend = NULL;
+
+	      FriendNode* fNode = friendHead;
+	      bool anotherFriend = false;
+
+	      //position is to keep track of
+	      int position = 0;
+	      int currentFriend = 0;
+
+	      //parse through each person's line in input.txt
+	      for(size_t i = 0; i < line.length(); i++){
+		if(line[i] == '\n')
+		  break;
+		if(line[i] == ','){
+		  position++;
+		  if(position > 3)
+		    anotherFriend = true;
+		}
+		else if(position == 0)
+		  name += line[i];
+		else if(position == 1){
+		  age += line[i];
+		}
+		else if(position == 2){
+		  occupation += line[i];
+		}
+		else{
+		  if(anotherFriend){
+		    FriendNode* node = new FriendNode;
+		    node->name = line[i];
+		    node->nextFriend = NULL;
+		    fNode->nextFriend = node;
+		    fNode = fNode->nextFriend;
+		    anotherFriend = false;
+		  }
+		  else{
+		    fNode->name += line[i];
+		  }
+		}
+	      }
+	      cout << "Inserting: " << name << endl;
+	      f->insert(name, age, occupation, numInserted);
+	      h->insert(name, numInserted, friendHead);
+	      b->insertRoot(name,numInserted);
+	      numInserted++;
+	    }
+	  }
+
 	else
 	  {
 	    cin.clear();
 	    cout << "Inputed string format was incorrect" << endl;
-	  }
+	  }	  
       }
-
   }
   catch(exception& ex){
     cerr << ex.what() << endl;
   }
 }
 
-void getFriendsProfileData(string name,Hashtable* h, FSeeker* f){
-  HashNode* person = h->lookUp(name);
-  if(person == NULL)
-    return;
-  FriendNode* friendHead = person->friendHead;
+  void getFriendsProfileData(string name,Hashtable* h, FSeeker* f){
+    HashNode* person = h->lookUp(name);
+    if(person == NULL)
+      return;
+    FriendNode* friendHead = person->friendHead;
   
-  cout << "Profile data on the friends of " << name << endl;
+    cout << "Profile data on the friends of " << name << endl;
 
-  while(friendHead != NULL){
-    string friendName = friendHead->name;
-    HashNode* friendHashN = h->lookUp(friendName);
-    if(friendHashN != NULL){
-      cout << f->getProfileData(friendHashN->profileDataPointer) << endl;
+    while(friendHead != NULL){
+      string friendName = friendHead->name;
+      HashNode* friendHashN = h->lookUp(friendName);
+      if(friendHashN != NULL){
+	cout << f->getProfileData(friendHashN->profileDataPointer) << endl;
+      }
+      friendHead = friendHead->nextFriend;
     }
-    friendHead = friendHead->nextFriend;
+
+    if(friendHead == NULL)
+      return;
   }
 
-  if(friendHead == NULL)
-    return;
-}
-
-void printSocialNetwork(Hashtable* h, FSeeker* f, int totalUsers){
-  for(int i = 0; i < totalUsers; i++){
-    string name = f->getName(i);
-    cout << f->getProfileData(i) << "," << h->getFriends(name) << endl;
+  void printSocialNetwork(Hashtable* h, FSeeker* f, int totalUsers){
+    for(int i = 0; i < totalUsers; i++){
+      string name = f->getName(i);
+      cout << f->getProfileData(i) << "," << h->getFriends(name) << endl;
+    }
   }
-}
 
 
 
